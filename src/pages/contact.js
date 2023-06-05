4e4ba704544e3cd54a05f71faeb4907eb25a30c7
Addisu <addisuhaile87@gmail.com>
2023-06-05 18:46:24 +0300

    Update the contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
   };
 
   return (
-    <div className="flex relative flex-col md:text-left md:flex-row  max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <div className="flex relative flex-col md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <div className="py-24 md:col-span-2 space-y-8">
         <div className="px-4 sm:px-0">
           <h3 className="text-2xl md:text-4xl text-center font-bold leading-6 text-gray-900">
@@ -16,73 +16,119 @@ const Contact = () => {
           </h3>
         </div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="overflow-hidden shadow sm:rounded-md">
+          <div className="overflow-hidden drop-shadow-2xl sm:rounded-xl">
             <div className="bg-white px-4 py-5 sm:p-6">
               <div className="grid grid-cols-6 gap-6">
                 <div className="col-span-6 sm:col-span-3">
-                  <input
-                    type="text"
-                    name="full-name"
-                    {...register("name", { required: true })}
-                    placeholder="Name"
-                    id="name"
-                    autoComplete="name"
-                    className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="name"
+                      {...register("name", { required: true })}
+                      placeholder=""
+                      id="name"
+                      autoComplete="name"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                      placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
 
                 <div className="col-span-6 sm:col-span-3">
-                  <input
-                    type="text"
-                    name="email-address"
-                    {...register("EmailAddress", { required: true })}
-                    placeholder="Email Address"
-                    id="email-address"
-                    autoComplete="email"
-                    className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                  <label
+                    htmlFor="name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Sender Email
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="email-address"
+                      {...register("SenderEmail", { required: true })}
+                      placeholder=""
+                      id="email-address"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                      placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
 
                 <div className="col-span-6 sm:col-span-3">
-                  <input
-                    type="text"
-                    name="full-name"
-                    {...register("FromName", { required: true })}
-                    placeholder="From Name"
-                    id="from-name"
-                    autoComplete="from-name"
-                    className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Receiver Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="receiver-name"
+                      {...register("ReceiverName", { required: true })}
+                      placeholder=""
+                      id="receiver-name"
+                      autoComplete="receiver-name"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                      placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
 
                 <div className="col-span-6 sm:col-span-3">
-                  <input
-                    type="text"
-                    name="email-address"
-                    {...register("EmailAddress", { required: true })}
-                    placeholder="Email Address"
-                    id="email-address"
-                    autoComplete="email"
-                    className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Receiver Email
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="text"
+                      name="email-address"
+                      {...register("Receiver Email", { required: true })}
+                      placeholder=""
+                      id="email-address"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                    placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
 
-                <div className="col-span-6">
-                  <textarea
-                    id="message"
-                    name="message"
-                    {...register("message", { required: true })}
-                    placeholder="Message"
-                    rows={4}
-                    className="mt-1 block w-full border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                  />
+                <div className="col-span-full">
+                  <label
+                    htmlFor="message"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Message
+                  </label>
+
+                  <div className="mt-2">
+                    <textarea
+                      id="message"
+                      name="message"
+                      {...register("message", { required: true })}
+                      placeholder=""
+                      rows={4}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                    placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
             <div className="bg-gray-50 px-4 py-3 text-center md:text-left sm:px-6">
               <button
                 type="submit"
-                className="inline-flex justify-center rounded-2xl border border-transparent bg-zinc-900 px-16 py-3 md:py-2 md:px-8 text-base font-medium text-white shadow-sm hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2"
+                className="inline-flex justify-center rounded-2xl border border-transparent bg-green-600 px-16 py-3 md:py-2 md:px-8 text-base font-medium text-white shadow-sm hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2"
               >
                 Send
               </button>
